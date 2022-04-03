1deac7a4c458b386882fc86826e43fc6d3efb7a3
sureshgadupu <sureshgkhyd@gmail.com>
2022-04-03 22:34:17 +1200

    first commit

--- src/main/java/dev/fullstackcode/tc/docker/TestcontainersDockerApplication.java
+++ src/main/java/dev/fullstackcode/tc/docker/TestcontainersDockerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"dev.fullstackcode.tc.docker.entity"})
 public class TestcontainersDockerApplication {
 
 	public static void main(String[] args) {
